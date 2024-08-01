078d653aaff014f5dff77bcd3ac8b1904dad71be
wJuanca <juancarlosturcios2001@gmail.com>
2024-07-31 19:22:09 -0600

    actualizacion

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import handlebars from 'vite-plugin-handlebars';
 import handlerBarsContext from './variables.js';
 
 export default defineConfig({
-  base: "/ProyectoFinalPW2/",
+  base: "/ProyectoFinal/",
   appType: 'mpa',
   build: {
     rollupOptions: {
